25a8520d023c2eea1d70f3b1639b622eb8812565
navyasingla <80400920+navyasingla@users.noreply.github.com>
2021-07-12 11:38:34 +0530

    Delete taskschema.js

--- models/taskschema.js
+++
@@ -1,9 +0,0 @@
-const  mongoose  = require("mongoose");
-
-const  Schema  =  mongoose.Schema;
-const  taskSchema  =  new Schema({
-   owner: String,
-   tasks:[String]
-});
-let  User  =  mongoose.model("Task", taskSchema);
-module.exports  = taskSchema;
